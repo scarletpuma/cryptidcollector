1019abc96b2ec7fa6ec8c5e7ba259bbc8029fe71
Scarlet Puma <scarlt.puma@gmail.com>
2022-01-25 14:50:52 -0500

    kalsdhfj

--- elements.js
+++ elements.js
@@ -1,4 +1,23 @@
 $(() => {
+  const backgroundImages = [
+  "imgs/woods1.jpeg",
+  "imgs/woods3.jpeg",
+  "imgs/woods4.jpeg",
+  "imgs/woods5.jpeg",
+  "imgs/woods6.jpeg",
+  "imgs/woods7.jpeg",
+  "imgs/woods8.jpeg",
+  "imgs/woods9.jpeg",
+  "imgs/woods10.jpeg",
+  "imgs/woods11.jpeg",
+  "imgs/woods12.jpeg",
+  "imgs/woods13.jpeg",
+];
+
+const getRandomImgURL = () => {
+  const randomIndex = Math.floor(Math.random() * backgroundImages.length);
+  return backgroundImages[randomIndex];
+};
   //query for html elements
   const $pcInfo = $(`#pcInfo`);
   const $options = $(`#options`);
@@ -10,11 +29,13 @@ $(() => {
   const $items = $(`#items`);
   const $cryptids = $(`#cryptids`);
   const $titleScreen = $(`#titlescreen`);
-    // $(window).on(`load` function () {
-    //   $titleScreen
-    //     .fadeIn(3000)
-    //     .delay(3000)
-    //     .fadeOut(3000);
-    // })
-    
+  // $pcInfo.hide().delay(9000).show()
+  // $options.hide().delay(9000).show()
+  // $narrative.hide().delay(9000).show()
+  $titleScreen
+    .hide()
+    .fadeIn(3000)
+    .delay(3000)
+    .fadeOut(3000);
+$("body").css("background-image", `url(${getRandomImgURL()})`);
 });
