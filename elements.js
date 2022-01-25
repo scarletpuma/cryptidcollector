682ca37aaaf2117c61fd89e05e5ce58d5a8ae487
Scarlet Puma <scarlt.puma@gmail.com>
2022-01-25 11:58:21 -0500

    elements

---
+++ elements.js
@@ -0,0 +1,12 @@
+$(() => {
+  const $pcInfo = $(`#pcInfo`);
+  const $options = $(`#options`);
+  const $narrative = $(`#narrative`);
+  const $name = $(`#pcname`);
+  const $health = $(`#health`);
+  const $hunger = $(`#hunger`);
+  const $food = $(`#food`);
+  const $items = $(`#items`);
+  const $cryptids = $(`#cryptids`);
+
+})
